b6106225823c0a4cd45c3ce4b40ad6a388bb9ae1
cnsheafe <cnsheafe@gmail.com>
2017-04-20 16:40:03 -0500

    added post recipes

--- server.js
+++ server.js
@@ -52,6 +52,20 @@ app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
 
+app.post('/recipes', jsonParser, function(req, res) {
+	const requiredFields = ['name', 'ingredients'];
+	for (let i=0; i<requiredFields.length; i++){
+		if(!(requiredFields[i] in req.body)){
+			const errMsg = `Missing ${requiredFields[i]} in request body`;
+			console.error(errMsg);
+			return res.status(400).send(errMsg);
+		}
+	}
+
+	const item = Recipes.create(req.body.name, req.body.ingredients);
+	res.status(201).json(item);
+});
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
